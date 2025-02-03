dd47ef13fbfed01820c96ed55099c1bb07ba8097
Jonghyeon Park <belline0124@gmail.com>
2025-02-03 12:58:14 +0900

    chore: Add package (Pull feat/character-resource-control branch changes) (READ THE DESCRIPTION)
    This duplicated changes can make hard merging feat/character-resource-control branch in the future.

---
+++ Assets/Scripts/Models/PlayerData/PlayerData.cs
@@ -0,0 +1,37 @@
+using ProtoBuf;
+
+[System.Serializable]
+[ProtoContract]
+public class PlayerData
+{
+    private static PlayerData _instance;
+    public static PlayerData Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new PlayerData();
+            }
+            return _instance;
+        }
+    }
+    public static PlayerData I
+    {
+        get
+        {
+            return Instance;
+        }
+    }
+
+    [ProtoMember(1)]
+    public string playerName;
+    [ProtoMember(2)]
+    public MasterAudioMixerModel masterAudioMixer;
+    
+    private PlayerData()
+    {
+        this.playerName = PlayerDataDefaults.PLAYER_NAME;
+        this.masterAudioMixer = new MasterAudioMixerModel();
+    }
+}
